3cf1029ca4a1d62d1d4aaf12b8ac2ba24141a085
Willian Goncalves <williangoncalves74@gmail.com>
2024-06-18 13:45:25 -0300

    criado cores padrões do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      'azul-ancora': '#023a5d',
+      'vermelho-ancora': '#d02131',
+      fundo: '#FCFCFF',
+    },
   },
   plugins: [],
 };
