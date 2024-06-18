99d6e2d21831aa4cb590f6e2d5b11f3b4100566f
Willian Goncalves <williangoncalves74@gmail.com>
2024-06-18 14:30:23 -0300

    feat aspect ratio e container defaut

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      aspectRatio: {
+        '9/16': '9/16',
+      },
+      container: {
+        center: true,
+        padding: '2rem',
+        screens: {
+          default: '768px',
+        },
+      },
+    },
     colors: {
       'azul-ancora': '#023a5d',
       'vermelho-ancora': '#d02131',
