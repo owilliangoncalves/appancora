39064030d26e79f0647f0abb732fdf8c3f30747e
Guilherme Nunes <123277481+Guisnu@users.noreply.github.com>
2024-06-20 13:46:49 -0300

    Estilizando teclado

--- src/components/teclado.jsx
+++ src/components/teclado.jsx
@@ -1,54 +1,50 @@
 import { useState } from 'react';
 
-export default function Teclado() {
-  // armazenando texto que o usuario escreveu no text area
-  const [inputTexto, setInputText] = useState('');
+export default function Teclado (){
 
-  // Adicionando letra a text area
-  const addLetras = (Letra) => {
-    setInputText(inputTexto + Letra);
-  };
+// armazenando texto que o usuario escreveu no text area
+const [inputTexto, setInputText] = useState('');
+console.log(inputTexto);
 
-  // limpar texto
-  const limpar = () => {
-    setInputText('');
-  };
+// Adicionando letra a text area
+const addLetras = (Letra) => {
+setInputText(inputTexto + Letra);
+};
 
-  return (
-    <div className='flex flex-col items-center'>
-      <textarea readOnly value={inputTexto} className='w-full h-12 text-lg' />
-      <div className='grid grid-cols-7 gap-2 mt-5'>
-        {[...Array(26)].map((_, index) => {
-          const letter = String.fromCharCode(index + 65); // Letras maiúsculas A-Z
-          return (
-            <button
-              key={letter}
-              onClick={() => addLetras(letter)}
-              className='p-2 text-base bg-blue-500 hover:bg-blue-700 text-white rounded'
-            >
-              {letter}
+// limpar texto
+const limpar = () => {
+setInputText('');
+};
+
+return (
+    <div className="flex flex-col items-center">
+    <textarea readOnly value={inputTexto} className="w-full h-12 text-lg mb-4" />
+    <div className="grid grid-cols-10 gap-2 mt-3 justify-items-center">
+        {/* Números de 0 a 9 */}
+        {[...Array(10)].map((_, index_numero) => {
+        const numero = index_numero; 
+        return (
+            <button key={numero} onClick={() => addLetras(numero.toString())} className="w-10 h-10 text-base bg-azul-ancora hover:bg-azul-ancora-muted text-white rounded">
+            {numero}
             </button>
-          );
+        );
         })}
-        <button
-          onClick={() => addLetras(' ')}
-          className='p-2 text-base bg-gray-300 hover:bg-gray-400 text-black rounded'
-        >
-          Espaço
-        </button>
-        <button
-          onClick={limpar}
-          className='p-2 text-base bg-red-500 hover:bg-red-600 text-white rounded'
-        >
-          Limpar
-        </button>
-        <button
-          onClick={() => alert(`Texto inserido: ${inputTexto}`)}
-          className='p-2 text-base bg-green-500 hover:bg-green-600 text-white rounded'
-        >
-          Confirmar
-        </button>
-      </div>
+        {/* Letras de A a Z */}
+        {'QWERTYUIOPASDFGHJKLÇZXCVBNM-'.split('').map((letra) => (
+            <button key={letra} onClick={() => addLetras(letra)} className="w-10 h-10 text-base bg-azul-ancora hover:bg-azul-ancora-muted text-white rounded">
+            {letra}
+            </button>
+        ))}
+
+        
+        
     </div>
-  );
+    <div className='flex mt-5 mb-5 gap-x-5'>
+        <button onClick={limpar} className="p-2 w-[100px] text-base bg-red-500 hover:bg-red-600 text-white rounded">Limpar</button>
+        <button onClick={() => addLetras(' ')} className="w-96 h-10  text-base bg-gray-300 hover:bg-gray-400 text-black rounded"></button>
+        <button onClick={() => alert(`Texto inserido: ${inputTexto}` )} className="p-2 w-[100px] text-base bg-green-500 hover:bg-green-600 text-white rounded">Confirmar</button>  
+    </div>
+
+    </div>
+    );
 }
